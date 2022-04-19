2c1781720df707b76ad9ef420b662f552d60ffc4
vitordrager1 <vitordrager09@gmail.com>
2022-04-19 00:08:52 -0300

    Modal concluida por completo, corrigido bugs e adicionado o restante das funcionalidades

--- rocketseat_node/public/scripts/main.js
+++ rocketseat_node/public/scripts/main.js
@@ -2,15 +2,35 @@ import Modal from './modal.js'
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //pegar todos os botoes que existe com a class check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button =>{//button armazena o objeto que o foreach traz dos 3 botoes existetes do checkbuttons ou seja tem3  botoes como é um for ele traz 1 botao de cada vez e armazena na button
     //adicionar o event listener
     //adiciona nos 3 botoes
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
-//pegar qnd o marcar como lido for clicado
 
+
+//quando o botao delete for clicado ele abre a modal
+const deleteButton = document.querySelectorAll(".actions a.delete");
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir ";
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescription.innerHTML =`Tem certeza que deseja ${text.toLowerCase()} esta pergunta ?`
+    modalButton.innerHTML= `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    //abrir modal
+    modal.open()
+}
