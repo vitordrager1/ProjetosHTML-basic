76959961a664a4e73a86343c843c4acde292fdcf
vitordrager1 <vitordrager09@gmail.com>
2022-04-22 00:30:27 -0300

    criado o banco de dados sqlite com as tabelas e feito o JS para criar salas com id aleatorio de 6 digitos

---
+++ rocketseat_node/src/db/init.js
@@ -0,0 +1,25 @@
+const Database = require ('./config')
+
+const initDb = {
+    async init(){ //async é o conceito de nao esperar o comando terminar de processar para seguir em frente, com async ele apenas vai lançando os comandos sem esperar um retorno
+        const db = await Database() // o await serve para forçar os comandos em js esperar um retorno do DB para assim seguir a frente.
+
+        await db.exec(`CREATE TABLE rooms(
+            id INTEGER PRIMARY KEY,
+            pass TEXT 
+        )`);//dentro do exec é onde vai o codigo sql para o db
+
+        await db.exec(`
+        CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            read INT 
+        )`);
+
+        await db.close()
+    }
+}
+
+initDb.init();
+
+
