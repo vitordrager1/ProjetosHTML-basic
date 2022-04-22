1c41dd5cfc623702cab48a5b3f300bd2abfd340f
vitordrager1 <vitordrager09@gmail.com>
2022-04-21 23:35:25 -0300

    criado o JS dos botoes da modal, reduzido a redundancia de codigo entre o index e o create-pass, criado os controllers question e room

--- rocketseat_node/src/server.js
+++ rocketseat_node/src/server.js
@@ -3,13 +3,16 @@ const route = require('./route');
 const server = express();
 const path = require('path')
 
-server.use(route)
 
 server.set('view engine', 'ejs')
 
 server.use(express.static("public"))
 
 server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
+
+server.use(route)
 
 server.listen(3000, ()=> console.log("RODANDO"))
 
