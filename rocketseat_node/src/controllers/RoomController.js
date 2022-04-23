63b8f68ebb0cadf5c6b84e4a8d21d1bbc748c37f
vitordrager1 <vitordrager09@gmail.com>
2022-04-23 17:40:53 -0300

    configurado os botoes de marcar como lido e excluir tambem feito a validacao das senhas

--- rocketseat_node/src/controllers/RoomController.js
+++ rocketseat_node/src/controllers/RoomController.js
@@ -4,13 +4,12 @@ module.exports = {
     async create(req, res){
         const db = await Database();
         const pass = req.body.password;
-        let roomId;
+        let roomId = "";
         let isRoom = true;
 
         while(isRoom){
             /* for que gera o id aleatorio*/
             for (var i = 0; i < 6; i++){
-                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
                 roomId += Math.floor(Math.random() * 10).toString()
             }
 
@@ -20,7 +19,7 @@ module.exports = {
             isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId)//a func some retorna true a primeira condição que seja satisfeita sendo assim n verifica todas as salas
             
             
-            /*inseri a sala no banco*/
+            /*inserir a sala no banco*/
             if(!isRoom){
                 await db.run(`INSERT INTO rooms (id, pass)
                 VALUES (
@@ -33,6 +32,20 @@ module.exports = {
 
         await db.close();
 
+        res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res){
+        const db = await Database();
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+    },
+
+    enter(req, res){
+        const roomId = req.body.roomId;
         res.redirect(`/room/${roomId}`)
     }
 }
--- rocketseat_node/src/route.js
+++ rocketseat_node/src/route.js
@@ -7,9 +7,15 @@ const route = express.Router();
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room"))
-//Formato que o formulario da modal tem que passat a informação
-route.post('/question/:room/:question/:action',questionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enterroom', RoomController.enter)
+
+
+
+//Formato que o formulario da modal tem que passar a informação
+route.post('/question/create/:room', questionController.create)
+route.post('/question/:room/:question/:action',questionController.index)
+
 
 module.exports = route
