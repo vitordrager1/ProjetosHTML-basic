ddb670f194f3d025168a3a6c0595ff75dc74bc74
vitordrager1 <vitordrager09@gmail.com>
2022-04-23 13:56:53 -0300

    adicionado a verificação para não cirar ids de salas iguais

--- rocketseat_node/src/controllers/RoomController.js
+++ rocketseat_node/src/controllers/RoomController.js
@@ -5,18 +5,31 @@ module.exports = {
         const db = await Database();
         const pass = req.body.password;
         let roomId;
-        for (var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        let isRoom = true;
+
+        while(isRoom){
+            /* for que gera o id aleatorio*/
+            for (var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+                /*verificar se o id ja existe*/
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+
+            isRoom = roomsExistIds.some(roomsExistIds => roomsExistIds === roomId)//a func some retorna true a primeira condição que seja satisfeita sendo assim n verifica todas as salas
+            
+            
+            /*inseri a sala no banco*/
+            if(!isRoom){
+                await db.run(`INSERT INTO rooms (id, pass)
+                VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
+       
         }
-
- 
-
-        await db.run(`INSERT INTO rooms (id, pass)
-        VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close();
 
